333d787ab763ef2ac35780bd6de4b5623c4283d1
Alan Clark <alan@thatscottishdatascientist.com>
2019-01-06 14:19:38 +0000

    Updates to team results summary

--- macros/teams/get_team_results.sql
+++ macros/teams/get_team_results.sql
@@ -1,9 +1,26 @@
-{% macro get_team_results(result) %}
+{% macro get_team_results(result, location) %}
     {{ log("Running macro get_team_results with arguments: result: " ~ result) }}
 
-    select team_api_id, count(result) as {{ result }}
+    select team_api_id, count(result) as {{ result }}{% if location in ['home', 'away'] %}_{{ location }}{% endif %}
     from {{ ref('match_results') }}
     where result = (select category from {{ ref('result_categories') }} where result = '{{ result }}')
+    {% if location in ['home', 'away'] %}
+        and home = {{ 1 if location == 'home' else 0 }}
+    {% endif %}
     group by team_api_id
+{% endmacro %}
 
+{% macro get_team_results_location(result, location, order) %}
+    {{ log("Running macro get_team_results_location with arguments: result: " ~ result ~ ", location: " ~ location ~ ", order: " ~ order) }}
+
+    select *
+    from
+    (
+        select team_api_id, match_api_id, date,
+        ROW_NUMBER() OVER (PARTITION BY team_api_id ORDER BY date {{ order }}) rn
+        from {{ ref('match_results') }}
+        where result = (select category from {{ ref('result_categories') }} where result = '{{ result }}')
+        and home = {{ 1 if location == 'home' else 0 }}
+    ) a
+    where rn=1
 {% endmacro %}
--- models/teams/team_results.sql
+++ models/teams/team_results.sql
@@ -1,17 +1,41 @@
-select r.*, w.win, l.loss, d.draw from (
+{%- set locations = ['home', 'away'] -%}
+{%- set results = ['win', 'loss', 'draw'] -%}
+{%- set orders = ['asc', 'desc'] -%}
+
+select r.*,
+{% for result in results -%}
+    r_{{ result }}.{{ result }} as {{ result }},
+    {%- for location in locations -%}
+        r_{{ result }}_{{ location }}.{{ result }}_{{ location }} as {{ result }}_{{ location }},
+        {%- for order in orders -%}
+            r_{{ result }}_{{ location }}_{{ order }}.match_api_id as {{ result }}_{{ location }}_{{ order }}_match_api_id,
+            r_{{ result }}_{{ location }}_{{ order }}.date::date as {{ result }}_{{ location }}_{{ order }}_date
+            {%- if not loop.last %}, {% endif %}{% if loop.last -%} {%- endif -%}
+        {%- endfor -%}
+        {%- if not loop.last %}, {% endif %}{% if loop.last -%} {%- endif -%}
+    {%- endfor -%}
+    {%- if not loop.last %}, {% endif %}{% if loop.last -%} {%- endif -%}
+{%- endfor %}
+from (
     select team_api_id, count(team_api_id) as matches_played, sum(home) as home_matches_played
     from {{ ref('match_results') }}
     group by team_api_id
 ) r
-join (
-    {{ get_team_results('loss') }}
-) l
-on r.team_api_id = l.team_api_id
-join (
-    {{ get_team_results('win') }}
-) w
-on r.team_api_id = w.team_api_id
-join (
-    {{ get_team_results('draw') }}
-) d
-on r.team_api_id = d.team_api_id
+{% for result in results %}
+    join (
+        {{ get_team_results(result, '') }}
+    ) r_{{ result }}
+    on r.team_api_id = r_{{ result }}.team_api_id
+    {% for location in locations %}
+        join (
+            {{ get_team_results(result, location) }}
+        ) r_{{ result }}_{{ location }}
+        on r.team_api_id = r_{{ result }}_{{ location }}.team_api_id
+        {% for order in orders %}
+            join (
+                {{ get_team_results_location(result, location, order) }}
+            ) r_{{ result }}_{{ location }}_{{ order }}
+            on r.team_api_id = r_{{ result }}_{{ location }}_{{ order }}.team_api_id
+        {% endfor %}
+    {% endfor %}
+{% endfor %}
