26de36c140d9a1b9886abfd9d7531cd39fc4df98
Alan Clark <alan@thatscottishdatascientist.com>
2019-01-06 16:02:03 +0000

    Extracting the number of wins, losses and draws for each player

--- models/players/player_data.sql
+++ models/players/player_data.sql
@@ -1,4 +1,4 @@
-select p.id, a.age, b.bmi, b.bmi_cat, c.match_count, c.first_played, c.last_played, cl.career_length from "Player" as p
+select p.id, a.age, b.bmi, b.bmi_cat, c.match_count, c.first_played, c.last_played, c.win, c.loss, c.draw, cl.career_length from "Player" as p
 left join {{ ref('player_age') }} as a
 on p.id = a.id
 left join {{ ref('player_bmi_cat') }} as b
--- models/players/player_match_count.sql
+++ models/players/player_match_count.sql
@@ -1,3 +1,19 @@
-select player_api_id, count(player_api_id) as match_count, min(date) as first_played, max(date) as last_played
-from {{ ref('match_players') }}
-group by player_api_id
+{%- set results = get_ref_column_data(ref('result_categories'), 'result')  -%}
+
+select r.player_api_id, r.match_count, r.first_played, r.last_played,
+{% for result in results %}
+    {{ result }}.{{ result }}{%- if not loop.last %}, {% endif %}{% if loop.last -%} {%- endif -%}
+{% endfor %}
+from (
+    select player_api_id, count(player_api_id) as match_count, min(date)::date as first_played, max(date)::date as last_played
+    from {{ ref('match_players') }}
+    group by player_api_id
+) r
+{% for result in results %}
+    left join (
+        select player_api_id, count(player_api_id) as {{ result }} from {{ ref('match_players') }}
+        where result = (select category from {{ ref('result_categories') }} where result = '{{ result }}')
+        group by player_api_id
+    ) {{ result }}
+    on r.player_api_id = {{ result }}.player_api_id
+{% endfor %}
