e4e155adf9d8cc8b2410f941dc2c59b0abf37b1e
Alan Clark <alan@thatscottishdatascientist.com>
2019-01-06 15:16:27 +0000

    Removing unused model, and improving match_players model

---
+++ models/matches/match_players.sql
@@ -0,0 +1,18 @@
+{%- set match_columns_text = match_columns|join(', ') -%}
+{%- set locations = get_ref_column_data(ref('match_categories'), 'category')  -%}
+
+select player_api_id, match_api_id, date::date as date, team_api_id, opposition_team_api_id, home, result from (
+{% for location in locations %}
+    {% for position in player_numbers() %}
+        (select {{location}}_player_{{position}} as player_api_id,
+        match_api_id, date,
+        {{ location }}_team_api_id as team_api_id,
+        {{ 'away' if location == 'home' else 'home' }}_team_api_id as opposition_team_api_id,
+        {{ 1 if location == 'home' else 0 }} as home,
+        {{ determine_match_result(location) }} as result
+        from "Match")
+        {% if not loop.last %}union all{% endif %}
+    {% endfor %}{% if not loop.last %} union all {% endif %}
+{% endfor %}
+) r
+where player_api_id is not null
--- models/players/player_match_count.sql
+++ models/players/player_match_count.sql
@@ -1,3 +1,3 @@
 select player_api_id, count(player_api_id) as match_count, min(date) as first_played, max(date) as last_played
-from {{ ref('match_player_conso') }}
+from {{ ref('match_players') }}
 group by player_api_id
